93e84c8fa45664bed182f993df265333f79503d5
laurenc206 <laurencavanaugh@laurens-mbp-2.mynetworksettings.com>
2024-10-16 01:40:26 -0700

    grid links mobile test

--- components/ui/grid-components.tsx
+++ components/ui/grid-components.tsx
@@ -177,24 +177,29 @@ export const Socials = () => {
 
                     
                    
-                        <a href="https://www.linkedin.com/in/cavanaugh-lc/" target="_blank" className=" bg-opacity-90 bg-slate-900 flex flex-row items-center rounded-md py-1 px-2"> 
+                        
+                            <div className="bg-opacity-90 bg-slate-900 flex items-center rounded-md py-1 px-2">
                             <div className="relative px-2 py-2 flex flex-row gap-x-2">
                                 <img src="/link.svg" className="h-[18px] w-[18px]" />
                             </div>
+                            <a href="https://www.linkedin.com/in/cavanaugh-lc/" target="_blank"> 
                             <div className="flex flex-row hover:underline gap-x-1">
                                 <text className="text-sm">
                                     /cavanaugh-lc
                                 </text>
                                 <CgExternal size={18} />
-                            </div>  
-                            </a>      
+                            </div> 
+                            </a> 
+                            </div>
+                           
                    
                  
 
                     
                                
-                        <a href="https://github.com/laurenc206/" target="_blank" className="bg-opacity-90 bg-slate-900 flex items-center rounded-md py-1 px-2"> 
-                            <div className="relative px-2 py-2 flex gap-x-2">
+                        <a href="https://github.com/laurenc206/" target="_blank" > 
+                            <div className="bg-opacity-90 bg-slate-900 flex items-center rounded-md py-1 px-2"> 
+                            <div className="px-2 py-2 flex gap-x-2">
                                 <img src="github2.svg" className="h-[18px] w-[18px]"/>
                             </div>
                             <div className="flex flex-row hover:underline gap-x-1">
@@ -203,8 +208,9 @@ export const Socials = () => {
                                 </text>
                                 <CgExternal size={18}/>
                             </div>
-                            </a>
-                     
+                            </div>
+                        </a>
+                        
                     
 
                     <div className=" bg-opacity-90 bg-slate-900 flex items-center rounded-md py-1 px-2">
